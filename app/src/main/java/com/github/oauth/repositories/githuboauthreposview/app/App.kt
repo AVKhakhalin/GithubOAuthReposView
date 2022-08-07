d9ec8a3635b37bd0119933b48efd01ef94fe026d
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-08-07 22:25:08 +0300

    Подключён Dagger.

--- app/src/main/java/com/github/oauth/repositories/githuboauthreposview/app/App.kt
+++ app/src/main/java/com/github/oauth/repositories/githuboauthreposview/app/App.kt
@@ -1,9 +1,17 @@
 package com.github.oauth.repositories.githuboauthreposview.app
 
 import android.app.Application
+import com.github.oauth.repositories.githuboauthreposview.di.components.AppComponent
+import com.github.oauth.repositories.githuboauthreposview.di.components.DaggerAppComponent
 
 class App: Application() {
     /** Исходные данные */ //region
+    // appComponent
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder()
+//            .appModule(AppModule(this)) // TODO: Разобраться в необходимости
+            .build()
+    }
     override fun onCreate() {
         super.onCreate()
         _instance = this
